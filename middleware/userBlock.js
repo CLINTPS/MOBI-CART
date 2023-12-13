c24a9cb3a64e99b1e980dc4816188ccf7d751b2a
CLINTPS <clintps1010@gmail.com>
2023-12-13 15:23:17 +0530

    second commit

--- middleware/userBlock.js
+++ middleware/userBlock.js
@@ -1,17 +1,17 @@
-const user=require("../model/user")
+const user = require("../model/user")
 
 
-const checkBlock= async(req,res,next   )=>{
+const checkBlock = async (req, res, next) => {
     // console.log(req.session.email);
-    const check=await user.findOne({email:req.session.email})
+    const check = await user.findOne({ email: req.session.email })
     // console.log("user block checking"+check);
-    if(check && check.status==false){
+    if (check && check.status == false) {
         console.log("user block done cannot access...........");
-        req.session.logged=false
-            res.redirect('/')
-    }else{
+        req.session.logged = false
+        res.redirect('/')
+    } else {
         next()
     }
 
 }
-module.exports=checkBlock;
+module.exports = checkBlock;
