f38617e65b1d7dd779252dda3bbd931a3ec2daaf
CLINTPS <clintps1010@gmail.com>
2023-12-13 15:32:32 +0530

    First branch commit

--- middleware/userBlock.js
+++ middleware/userBlock.js
@@ -2,9 +2,7 @@ const user = require("../model/user")
 
 
 const checkBlock = async (req, res, next) => {
-    // console.log(req.session.email);
     const check = await user.findOne({ email: req.session.email })
-    // console.log("user block checking"+check);
     if (check && check.status == false) {
         console.log("user block done cannot access...........");
         req.session.logged = false
@@ -14,4 +12,6 @@ const checkBlock = async (req, res, next) => {
     }
 
 }
+
+
 module.exports = checkBlock;
